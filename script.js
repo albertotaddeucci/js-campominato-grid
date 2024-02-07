ac932523309bff41cde97f88039df88f3a739625
alberto <alberto.taddeucci@gmail.com>
2024-02-07 17:36:18 +0100

    fix griglie

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 //variabile difficolta
 let difficoulty;
 
-
+const containerElement = document.getElementById("container");
 
 
 
@@ -18,65 +18,54 @@ buttonPlay.addEventListener("click",function(){
         let difficoulty = 100
 
         createGrid(difficoulty);
+
+
     } else if (selectDiff == 2){
         difficoulty = 81
         createGrid(difficoulty);
 
+
     } else if (selectDiff == 3){
         difficoulty = 49
         createGrid(difficoulty);
+
     }
        
+        
     
-        
-        
-        // for(let i = 0; i < 10; i++) {
-            
-            //     const newRow = document.createElement("div");
-            //     newRow.classList.add("row", "row-cols-auto","flex-wrap", "justify-content-center", "border");
-            
-            
-            //     containerElement.append(newRow);
-            
-            
-            
-            
-            
-            // }
-            
-        
-        
 })
 
-    
-    function createGrid(difficoulty){
 
-        const gridElement = document.getElementById("grid")
-        // const containerElement = document.getElementById("container")
-        const randomNumbersArray = getRandomNumbersArray();
+function createGrid(difficoulty){
+    
+    const gridElement = document.getElementById("grid")
+    // const containerElement = document.getElementById("container")
+    const randomNumbersArray = getRandomNumbersArray();
+    
+    for(let i = 0; i < difficoulty; i++){
+        const newElement = document.createElement("div");
+        newElement.classList.add("col-1", "border-black", "border" ,"square");
         
-        for(let i = 0; i < difficoulty; i++){
-            const newElement = document.createElement("div");
-            newElement.classList.add("col-1", "border-black", "border" ,"square");
+        gridElement.append(newElement);
+        
+        // al click aggiungo classe active e numero in console
+        newElement.addEventListener("click", function() {
             
-            gridElement.append(newElement);
-        
-            // al click aggiungo classe active e numero in console
-            newElement.addEventListener("click", function() {
-        
-                this.classList.toggle("active");
-                console.log(randomNumbersArray[i]);    
-                        
-            });
+            this.classList.toggle("active");
+            console.log(randomNumbersArray[i]);    
+            
+        });
+
+        if (difficoulty<50){
+            newElement.classList.add("square3")
+        } else if(difficoulty<90){
+            newElement.classList.add("square2")
+
         }
 
-        return gridElement;
     }
- 
-    //creazione griglia
     
-
-
+}
 
 
 
