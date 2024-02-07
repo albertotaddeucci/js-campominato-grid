596b0a80de0acdff1724a713d854fd013c363072
alberto <alberto.taddeucci@gmail.com>
2024-02-07 20:15:50 +0100

    aggiunta stile

--- script.js
+++ script.js
@@ -2,52 +2,51 @@
 
 
 //variabile difficolta
-let difficoulty = 100;
 
 const gridElement = document.getElementById("grid")
-const randomNumbersArray = getRandomNumbersArray();
-
-
-
-
 
 //bottone per giocare
 const buttonPlay = document.getElementById("button-play");
 
+let randomNumbersArray;
+let cellNum;
+
 buttonPlay.addEventListener("click",function(){
     let selectDiff = document.getElementById("select").value;
     gridElement.innerText = ""
-    
-    
+        
     
     if (selectDiff == 1){
-        difficoulty = 100      
-        createGrid(difficoulty);
+        cellNum = 100    
+        randomNumbersArray = getRandomNumbersArray(cellNum)  
+        createGrid(cellNum,10);
+
         
     } else if (selectDiff == 2){
-        difficoulty = 81
-        createGrid(difficoulty);
+        cellNum = 81
+        randomNumbersArray = getRandomNumbersArray(cellNum)  
+        createGrid(cellNum,9);
         
         
     } else if (selectDiff == 3){
-        difficoulty = 49
-        createGrid(difficoulty);
-        
-    }      
+        cellNum = 49
+        randomNumbersArray = getRandomNumbersArray(cellNum)  
+        createGrid(cellNum,7);
+    }
     
     
 })
 
 
 
-
 //funzione crea griglia
-function createGrid(difficoulty){
+function createGrid(cellNum,numberCol){
     
     
-    for(let i = 0; i < difficoulty; i++){
+    for(let i = 0; i < cellNum; i++){
         const newElement = document.createElement("div");
-        newElement.classList.add("col-1", "border-black", "border" ,"square");
+        newElement.classList.add("col-auto", "border-black", "border" ,"square");
+        newElement.style.width = `calc(100%/${numberCol})`;
         
         gridElement.append(newElement);
         
@@ -58,29 +57,24 @@ function createGrid(difficoulty){
             console.log(randomNumbersArray[i]);    
             
         });
-        
-        if (difficoulty<50){
-            newElement.classList.add("square3")
-        } else if(difficoulty<90){
-            newElement.classList.add("square2")
 
-        }
-
+    
     }
+            
     
 }
 
 
 
 //funzione per array random    
-function getRandomNumbersArray() {
+function getRandomNumbersArray(maxNumArray) {
 
     const numbersArray = [];
 
-    while (numbersArray.length < difficoulty) {
+    while (numbersArray.length < maxNumArray) {
 
         // Inserisco il numero solo se non è già presente
-        const newNumber = generateRandomNumber(difficoulty);
+        const newNumber = generateRandomNumber(maxNumArray);
 
         if( ! numbersArray.includes(newNumber) ) {
 
