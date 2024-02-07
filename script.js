209298d8685f1136899a72ceec3b21f873911b0d
alberto <alberto.taddeucci@gmail.com>
2024-02-07 16:32:13 +0100

    aggiunta select (wip:griglie)

--- script.js
+++ script.js
@@ -1,50 +1,85 @@
 
 
+
 //variabile difficolta
-let difficoulty = 100
+let difficoulty;
+
+
+
+
 
 //bottone per giocare
 const buttonPlay = document.getElementById("button-play");
 
 buttonPlay.addEventListener("click",function(){
+    let selectDiff = document.getElementById("select").value;
+    
+    if (selectDiff == 1){
+        let difficoulty = 100
 
-    //creazione griglia
-    const gridElement = document.getElementById("grid")
-    // const containerElement = document.getElementById("container")
-    const randomNumbersArray = getRandomNumbersArray();
-    
-    for(let i = 0; i < 100; i++){
-        const newElement = document.createElement("div");
-        newElement.classList.add("col-1", "border-black", "border" ,"square");
-        
-        gridElement.append(newElement);
-    
-        // al click aggiungo classe active e numero in console
-        newElement.addEventListener("click", function() {
-    
-            this.classList.toggle("active");
-            console.log(randomNumbersArray[i]);    
-                    
-        });
+        createGrid(difficoulty);
+    } else if (selectDiff == 2){
+        difficoulty = 81
+        createGrid(difficoulty);
+
+    } else if (selectDiff == 3){
+        difficoulty = 49
+        createGrid(difficoulty);
     }
-    
-    
-    // for(let i = 0; i < 10; i++) {
-
-    //     const newRow = document.createElement("div");
-    //     newRow.classList.add("row", "row-cols-auto","flex-wrap", "justify-content-center", "border");
        
-
-    //     containerElement.append(newRow);
-        
-
-        
-        
     
-    // }
-
-
+        
+        
+        // for(let i = 0; i < 10; i++) {
+            
+            //     const newRow = document.createElement("div");
+            //     newRow.classList.add("row", "row-cols-auto","flex-wrap", "justify-content-center", "border");
+            
+            
+            //     containerElement.append(newRow);
+            
+            
+            
+            
+            
+            // }
+            
+        
+        
 })
+
+    
+    function createGrid(difficoulty){
+
+        const gridElement = document.getElementById("grid")
+        // const containerElement = document.getElementById("container")
+        const randomNumbersArray = getRandomNumbersArray();
+        
+        for(let i = 0; i < difficoulty; i++){
+            const newElement = document.createElement("div");
+            newElement.classList.add("col-1", "border-black", "border" ,"square");
+            
+            gridElement.append(newElement);
+        
+            // al click aggiungo classe active e numero in console
+            newElement.addEventListener("click", function() {
+        
+                this.classList.toggle("active");
+                console.log(randomNumbersArray[i]);    
+                        
+            });
+        }
+
+        return gridElement;
+    }
+ 
+    //creazione griglia
+    
+
+
+
+
+
 
 
 
